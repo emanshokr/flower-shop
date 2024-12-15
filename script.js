c6e33940dfa2b73d5beca80a13f1538e51a4dbca
emanshokr <emanshokr201@gmail.com>
2024-12-15 21:33:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const navlinks = document.querySelector(".nav-links");
+    navlinks.classList.toggle("active");
+}
+
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((accordion) => {
+    accordion.addEventListener("click", () => {
+        if (accordion.classList.contains("active")) {
+            accordion.classList.remove("active");
+        } else {
+            accordion.classList.add("active");
+        }
+    });
+});
